6330e0ac84ddcc239d1a7fbb54c5bce7f2cfc0f4
Callum Slaney-Flett <callum@mobi-market.co.uk>
2021-08-05 15:30:05 +0100

    What made me do this?

--- src/Checkmend.php
+++ src/Checkmend.php
@@ -188,7 +188,7 @@ class Checkmend
      * @throws CheckmendInvalidRequestBody
      * @throws GuzzleException
      */
-    protected function sendAPIRequest(array $dataPackage, string $apiEndPoint, bool $incReseller = true): ?stdClass
+    protected function sendAPIRequest(array $dataPackage, string $apiEndPoint, bool $incReseller = true): stdClass
     {
         if ($this->reseller === true && $incReseller === true) {
             $dataPackage['moreinformation '] = 'Y';
@@ -213,11 +213,9 @@ class Checkmend
             $r = new stdClass();
             $r->result = 'complete';
             $r->certid = 'Check certificate email for certificate.';
-
-            return $r;
         }
 
-        return null;
+        return $r;
     }
 
     /**
